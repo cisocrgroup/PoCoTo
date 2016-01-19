9644f08a871b51afc1efd87ee736c5d83105dde6
flo <finkf@cis.uni-muenchen.de>
2016-01-19 10:31:42 +0100

    add isSuspicious method and add documentation.

--- CorrectionBackend/src/jav/correctionBackend/export/Char.java
+++ CorrectionBackend/src/jav/correctionBackend/export/Char.java
@@ -11,19 +11,66 @@ package jav.correctionBackend.export;
  */
 public interface Char {
 
+    /**
+     * Returns the BoundingBox of this Char.
+     *
+     * @return the BoundingBox of the Char
+     */
     public BoundingBox getBoundingBox();
 
-    public Char getPrev();
-
-    public Char getNext();
-
-    public void delete();
-
-    public void substitute(String c);
-
+    /**
+     * Get the characters of this Char.
+     *
+     * @return The characters of this Char.
+     */
     public String getChar();
 
+    /**
+     * Returs whether this Character is suspicious (Abbyy).
+     *
+     * @return true if this Character is suspicious.
+     */
+    public String isSuspicious();
+
+    /**
+     * Returns the previous Char or null if it is the first Char on the line.
+     *
+     * @return the previous Char or null.
+     */
+    public Char getPrev();
+
+    /**
+     * Return the next Char or null if it is the last Char on the line.
+     *
+     * @return the next Char or null.
+     */
+    public Char getNext();
+
+    /**
+     * Remove this character from the line.
+     */
+    public void delete();
+
+    /**
+     * Substitute this Character with another.
+     *
+     * @param c the new Character.
+     */
+    public void substitute(String c);
+
+    /**
+     * Create a new Char instance and append it after this.
+     *
+     * @param c the data of the new Char.
+     * @return the new Char that was appended.
+     */
     public Char append(String c);
 
+    /**
+     * Create a new Character and put it before this Char.
+     *
+     * @param c the data of the new Char.
+     * @return the new Char that was prepended.
+     */
     public Char prepend(String c);
 }
