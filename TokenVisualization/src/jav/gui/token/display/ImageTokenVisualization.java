0e274ff7c3af4266aa06fcc091d8f80d7fcf1741
flo <finkf@cis.uni-muenchen.de>
2016-01-27 16:34:39 +0100

    change font `FreeSerif` to display coptic letters

--- TokenVisualization/src/jav/gui/token/display/ImageTokenVisualization.java
+++ TokenVisualization/src/jav/gui/token/display/ImageTokenVisualization.java
@@ -11,34 +11,35 @@ import javax.swing.Box;
 import javax.swing.BoxLayout;
 
 /**
- *Copyright (c) 2012, IMPACT working group at the Centrum für Informations- und Sprachverarbeitung, University of Munich.
- *All rights reserved.
-
- *Redistribution and use in source and binary forms, with or without
- *modification, are permitted provided that the following conditions are met:
-
- *Redistributions of source code must retain the above copyright
- *notice, this list of conditions and the following disclaimer.
- *Redistributions in binary form must reproduce the above copyright
- *notice, this list of conditions and the following disclaimer in the
- *documentation and/or other materials provided with the distribution.
-
- *THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
- *IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
- *TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
- *PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- *HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- *SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- *LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- *DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- *THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- *(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- *OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
- * This file is part of the ocr-postcorrection tool developed
- * by the IMPACT working group at the Centrum für Informations- und Sprachverarbeitung, University of Munich.
- * For further information and contacts visit http://ocr.cis.uni-muenchen.de/
- * 
+ * Copyright (c) 2012, IMPACT working group at the Centrum für Informations- und
+ * Sprachverarbeitung, University of Munich. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer. Redistributions in binary
+ * form must reproduce the above copyright notice, this list of conditions and
+ * the following disclaimer in the documentation and/or other materials provided
+ * with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This file is part of the ocr-postcorrection tool developed by the IMPACT
+ * working group at the Centrum für Informations- und Sprachverarbeitung,
+ * University of Munich. For further information and contacts visit
+ * http://ocr.cis.uni-muenchen.de/
+ *
  * @author thorsten (thorsten.vobl@googlemail.com)
  */
 public class ImageTokenVisualization extends TokenVisualization {
@@ -49,7 +50,7 @@ public class ImageTokenVisualization extends TokenVisualization {
     public ImageTokenVisualization(BufferedImage bi, Token t, int fontSize) {
         super();
         this.tokenID = t.getID();
-        this.init( t, fontSize, bi);
+        this.init(t, fontSize, bi);
     }
 
     private void init(Token t, int fontSize, BufferedImage bi) {
@@ -66,8 +67,8 @@ public class ImageTokenVisualization extends TokenVisualization {
 
         this.tokenTextLabel = new TokenTextLabel(t);
         this.tokenTextLabel.setBackground(Color.white);
-        this.tokenTextLabel.setFont(new Font("DejaVu Sans", Font.PLAIN, fontSize));
-        
+        this.tokenTextLabel.setFont(getFont(fontSize));
+
         this.add(Box.createVerticalGlue());
 
         this.setFontSize(fontSize);
@@ -139,15 +140,32 @@ public class ImageTokenVisualization extends TokenVisualization {
     }
 
     @Override
-    public void update( String s ) {
-        super.update( s );
+    public void update(String s) {
+        super.update(s);
         tvm.setTokenVisualizationStyle(instance);
         this.calculateSizeNormMode();
         this.revalidate();
     }
 
-    public void update( BufferedImage bi, String s ) {
-        this.update( s );
+    public void update(BufferedImage bi, String s) {
+        this.update(s);
         this.setImage(bi);
+    }
+
+    private Font getFont(int fontSize) {
+//        try (InputStream is = new FileInputStream(
+//                new File("/home/flo/downloads/freefont-20120503/FreeSerif.ttf")
+//        )) {
+//            Font font = Font.createFont(Font.TRUETYPE_FONT, is);
+//            GraphicsEnvironment ge
+//                    = GraphicsEnvironment.getLocalGraphicsEnvironment();
+//            ge.registerFont(font);
+//            return font;
+//        } catch (IOException | FontFormatException e) {
+//            Log.error(this, e);
+//        }
+
+        return new Font("FreeSerif", Font.PLAIN, fontSize);
+        //return new Font("DejaVu Sans", Font.PLAIN, fontSize);
     }
 }
